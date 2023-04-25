212161fb8d9b0c5941f336e5b12e64044583a11d
Tom Brown <tom@nworbmot.org>
2023-04-25 18:34:23 +0200

    more reporting on costs

--- summarise_network.py
+++ summarise_network.py
@@ -19,12 +19,19 @@ s.loc["objective"] = n.objective
 s.loc["lcoe"] = s.loc["objective"]/s.loc["total_load"]
 
 
-for name,attr in [("generators","p"),("links","p"),("stores","e")]:
+stats = n.statistics(aggregate_time="sum").groupby(level=1).sum()
+
+stats["Total Expenditure"] = stats[["Capital Expenditure","Operational Expenditure"]].sum(axis=1)
+
+for name,full_name in [("capex","Capital Expenditure"),("opex","Operational Expenditure"),("totex","Total Expenditure"),("capacity","Optimal Capacity")]:
     s = pd.concat((s,
-                   getattr(n,name)[attr + "_nom_opt"]))
+                   stats[full_name].rename(lambda x: f"{x} {name}")))
 
 s = pd.concat((s,
-               n.statistics().loc["Generator"]["Curtailment"].rename(lambda x: f"curtailment {x}")))
+               n.statistics().loc["Generator"]["Curtailment"].rename(lambda x: f"{x} curtailment")))
+
+s = pd.concat((s,
+               n.statistics().groupby(level=1).sum()["Capacity Factor"].rename(lambda x: f"{x} capacity factor")))
 
 s = pd.concat((s,
                n.buses_t.marginal_price.mean().rename(lambda x: f"mean price {x}")))
@@ -54,6 +61,16 @@ for c in n.iterate_components(n.branch_components):
         mv = (c.pnl["p"+end][items].multiply(n.buses_t.marginal_price["electricity"], axis=0).sum()/c.pnl["p"+end][items].sum()).groupby(c.df.loc[items,'carrier']).mean()
         s = pd.concat((s,
                        mv.rename(lambda x: x+ " mv").replace([np.inf, -np.inf], np.nan).dropna()))
+
+
+#shares of elec
+ts = pd.concat([(-1)**i*n.links_t["p"+str(i)][n.links.index[n.links["bus"+str(i)].map(n.buses.carrier) == "electricity"]] for i in range(3)]
+               + [n.generators_t.p[n.generators.index[n.generators.bus.map(n.buses.carrier) == "electricity"]]],
+               axis=1)
+ts["load"] = n.loads_t.p["load"]
+
+s = pd.concat((s,
+               (ts.sum()/ts["load"].sum()).rename(lambda x: x+ " share")))
 
 
 s.loc["status"] = n.status
